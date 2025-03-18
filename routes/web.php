aa448c94a5c28e15d7483c845dc5bf4a4c391e48
jouxingngo <jouxing04@gmail.com>
2025-03-18 14:28:18 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\CompanyAboutController;
+use App\Http\Controllers\CompanyStatisticController;
+use App\Http\Controllers\OurTeamController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectClientController;
+use App\Http\Controllers\TestimonialController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::middleware('can:manage statistics')->group(function () {
+            Route::resource('statistics', CompanyStatisticController::class);
+        });
+
+        Route::middleware('can:manage products')->group(function () {
+            Route::resource('products', ProductController::class);
+        });
+
+        Route::middleware('can:manage testimonials')->group(function () {
+            Route::resource('testimonials', TestimonialController::class);
+        });
+
+        Route::middleware('can:manage clients')->group(function () {
+            Route::resource('clients', ProjectClientController::class);
+        });
+
+        Route::middleware('can:manage teams')->group(function () {
+            Route::resource('teams', OurTeamController::class);
+        });
+
+        Route::middleware('can:manage abouts')->group(function () {
+            Route::resource('abouts', CompanyAboutController::class);
+        });
+
+        Route::middleware('can:manage appointments')->group(function () {
+            Route::resource('appointments', AppointmentController::class);
+        });
+        
+    });
+});
+
+require __DIR__ . '/auth.php';
